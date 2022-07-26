cef96bae18259cba891ea89c056677864d47774d
Roa Mohamed <70070721+RoaPro9@users.noreply.github.com>
2022-07-26 16:29:14 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,22 +13,21 @@ recognition.continuous = false;
 
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
-var langOp = 0;
-document.getElementById("language").onchange = function () {
-    var value = document.getElementById("language").value;
-    langOp = value;
-}
+var langOp = 0; const obniz = new Obniz("OBNIZ_ID_HERE");
 
+obniz.onconnect = async function () {
+    const servo = obniz.wired("ServoMotor", { gnd: 0, vcc: 1, signal: 2 });
 
-
-
-click_to_convert.addEventListener('click', function () {
-    if (langOp == 1) { recognition.lang = 'ar-SA'; }
-    else if (langOp == 2) { recognition.lang = 'en-US'; }
-    else { recognition.lang = 'en-US'; }
-    recognition.start();
    
+    click_to_convert.addEventListener('click', function () {
 
+        if (langOp == 1) { recognition.lang = 'ar-SA'; }
+        else if (langOp == 2) { recognition.lang = 'en-US'; }
+        else { recognition.lang = 'en-US'; }
+
+       
+        recognition.start();
+    
 
         recognition.addEventListener('result', e => {
             const transcript = Array.from(e.results)
@@ -39,8 +38,19 @@ click_to_convert.addEventListener('click', function () {
             convert_text.innerHTML = transcript;
 
         });
-    
+        recognition.addEventListener = async event => {
+            document.getElementById("voice").innerHTML = transcript;
+            let angle_list = [40, 140, 40, 140];
+            if (event.results[0][0].transcript == "Turn right") {
+                for (let i = 0; i < angle_list.length; i++) {
+                    servo.angle(angle_list[i]);
+                    await obniz.wait(1000);
+                }
+            }
+        };
 
-        
+     
+  
     
-});
+    });
+}
